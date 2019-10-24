c2886430cfe4c20dc2e25d66953d25089f66f603
Geetha <Geetha@192.168.100.5>
2019-10-24 20:22:23 +0530

    test commit

---
+++ PageObject/src/test/java/NewTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class NewTest {
+ 
+ public WebDriver driver;
+ 
+  @Test
+  public void openMyBlog() {
+ driver.get("https://www.softwaretestingmaterial.com/");
+  }
+  
+  @BeforeClass
+  public void beforeClass() {
+   
+   System.setProperty("webdriver.gecko.driver", "C:\\Users\\Geetha\\eclipse-workspace\\PageObject\\src\\Executables\\chromedriver.exe");
+   driver = new FirefoxDriver();
+   
+  }
+ 
+  @AfterClass
+  public void afterClass() {
+   driver.quit();
+  }
+ 
+}
+
